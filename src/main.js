b986fe8971c0101f2e59034809112b97571280be
Shailen <shailen@gettruck.co.za>
2019-03-24 12:07:40 +0200

    Moved plugin into root dir

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
-import { plugin } from './vue-vuex-plugin';
+import { plugin } from '../vue-vuex-plugin';
 
 Vue.config.productionTip = false;
 
--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { createMutations } from './vue-vuex-plugin';
+import { createMutations } from '../vue-vuex-plugin';
 
 Vue.use(Vuex);
 
