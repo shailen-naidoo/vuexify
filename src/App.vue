2359aa018ef8b9461720476a0d6cc6d8f1881a23
Shailen <shailen@gettruck.co.za>
2019-03-24 11:56:30 +0200

    Added linter for Vue

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,13 @@
     <input v-model="name" type="text">
     <input v-model="surname" type="text">
     <p>{{ name + " " + surname }}</p>
-    <test/>
-  </div>
 </template>
 
 <script>
-import Test from "./components/Test.vue"
-
 export default {
   vuex: {
-    name: "name",
-    surname: "surname"
+    name: 'name',
+    surname: 'surname',
   },
-  components: {
-    Test
-  }
-}
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
-import Vuex from "./plugins/vuex";
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store';
+import Vuex from './plugins/vuex';
 
 Vue.config.productionTip = false;
 
@@ -9,5 +9,5 @@ Vue.use(Vuex);
 
 new Vue({
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
